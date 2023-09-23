3d6d5aecb932efa865e1ad3f42dbecacce39929d
Ethan4269875321 <ethansevenster1005@gmail.com>
2023-09-23 07:40:41 -0700

    Markdown previewer

---
+++ index.js
@@ -0,0 +1,22 @@
+const defaultText = `# heading
+## sub heading
+
+Here is my an h1 tag, \`<h1></h1>\`
+
+\`\`\`
+function codeBlock  {
+return 'some code'
+};
+\`\`\`
+
+- First item
+- Second item
+- Third item
+- Fourth item
+
+**Bold**!!!
+
+>A blockquote
+
+![My image]
+(https://cdn.freecodecamp.org/curriculum/css-cafe/coffee.jpg)`;
